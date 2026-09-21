0a32a00a80569e4cf17a226fd6b691cdb7d7325b
FabianAG <faguero@gmail.com>
2026-09-21 17:15:54 -0300

    feat(application): expose referenced actors to flavor projectors

--- src/agora/application/extensions.py
+++ src/agora/application/extensions.py
@@ -7,6 +7,7 @@ from dataclasses import dataclass, field
 from typing import Protocol
 
 from agora.application.dto import (
+    ActorSummary,
     ClarificationsProjection,
     LifecycleProjection,
     SerializableDTO,
@@ -40,6 +41,7 @@ class FlavorProjectionContext(SerializableDTO):
     traceability: TraceabilitySummary
     sessions: tuple[SessionSummary, ...]
     usage: UsageSummaryProjection
+    actors: tuple[ActorSummary, ...] = ()
     schema: str = field(default="agora/application/flavor-projection-context/v1", init=False)
 
 
